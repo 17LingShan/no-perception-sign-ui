7a91251e1e4882d79c49e93b92d906d3efaaab8a
17LingShan <huangruofeng9@gmail.com>
2022-11-11 23:07:00 +0800

    完成了student的api，todo:1.完成所有api配置；2.完成attendance页面

--- src/api/student.js
+++ src/api/student.js
@@ -1,13 +1,31 @@
 import { requestService } from '@/utils/request'
 
 const api = {
-  studentAttendance: '/user/inquire_attendance'
+  attendance: '/user/inquire_attendance',
+  joinCourse: '/user/join_course',
+  quitCourse: '/user/quit_course',
+  inquireJoinCourse: '/user/inquire_joincourse'
 }
 
 export function attendance (parms) {
   return requestService({
-    url: api.studentAttendance,
+    url: api.attendance,
     method: 'get',
     data: parms
+  })
+}
+
+export function joinCourse (parms) {
+  return requestService({
+    url: api.joinCourse,
+    method: 'post',
+    data: parms
+  })
+}
+
+export function inquireJoinCourse () {
+  return requestService({
+    url: api.inquireJoinCourse,
+    method: 'post'
   })
 }
--- src/views/student/Attendance.vue
+++ src/views/student/Attendance.vue
@@ -2,14 +2,11 @@
   <div>Attendance</div>
 </template>
 <script setup>
-import { attendance } from '@/api/student'
+import { attendance, inquireJoinCourse } from '@/api/student'
 import { onMounted } from 'vue';
 
 onMounted(() => {
-  const sutdent = {
-    student_id: 2000300115
-  }
-  attendance(JSON.stringify(sutdent)).then((res) => {
+  inquireJoinCourse().then((res) => {
     console.log(res)
   })
 })
